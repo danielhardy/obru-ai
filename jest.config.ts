1db50dc730d0aeed6dfa131a8dbc9789743249c4
Daniel Hardy <daniel@danielhardy.com>
2025-08-16 11:41:54 -0600

    Ignored deno tests

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/tests"],
+  testPathIgnorePatterns: ["/node_modules/", "deno\\.test\\.(ts|js)$"],
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.ts"],
   coverageDirectory: "coverage",
